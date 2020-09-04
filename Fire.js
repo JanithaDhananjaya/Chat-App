b5a094de91c650583251a3e5feee0a1fd43db369
Janitha Silva <Janitha.Silva@dialog.lk>
2020-09-04 23:31:02 +0530

    config firebase

--- Fire.js
+++ Fire.js
@@ -1,12 +1,75 @@
-import Firebase from "firebase";
+import firebase from "firebase";
 
-class Fire{
+class Fire {
     constructor() {
         this.init();
         this.checkAuth();
     }
 
-    init=()=> {
+    init = () => {
+        if (!firebase.apps.length) {
+            firebase.initializeApp(
+                {
+                    apiKey: 'AIzaSyCxTuKWIHwBQdKTqa8jLT3-ceOKAN5QYnE',
+                    authDomain: 'chatapp-b5c5d.firebaseapp.com',
+                    databaseURL: 'https://chatapp-b5c5d.firebaseio.com',
+                    projectId: 'chatapp-b5c5d',
+                    storageBucket: 'chatapp-b5c5d.appspot.com',
+                    messagingSenderId: '468541121822',
+                    appId: '1:468541121822:android:0266074d625a29b8c3d50f',
+                }
+            )
+        }
+    };
 
+    checkAuth = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            if (!user) {
+                firebase.auth().signInAnonymously();
+            }
+        })
+    };
+
+    send = messages => {
+        messages.forEach(item => {
+            const message = {
+                text: item.text,
+                timestamp: firebase.database.ServerValue.TIMESTAMP,
+                user: item.user
+            };
+
+            this.db.push(message);
+        })
+    };
+
+    parse = message => {
+        const {user, text, timestamp} = message.val();
+        const {key: _id} = message;
+        const createdAt = new Date(timestamp);
+
+        return {
+            _id,
+            createdAt,
+            text,
+            user
+        };
+    };
+
+    get = callback => {
+        this.db.on("child_added", snapshot => callback(this.parse(snapshot)))
+    };
+
+    off() {
+        this.db.off();
+    };
+
+    get uid() {
+        return (firebase.auth().currentUser || {}).uid;
+    }
+
+    get db() {
+        return firebase.database().ref("messages");
     }
 }
+
+export default new Fire();
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,21 +1,41 @@
-import React,{Component} from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React, {Component} from "react";
+import {View, Text, StyleSheet, Platform, KeyboardAvoidingView, SafeAreaView} from "react-native";
+import {GiftedChat} from "react-native-gifted-chat";
+import Fire from '../Fire';
 
-export default class ChatScreen extends Component{
-    render() {
-        return(
-            <View style={style.container}>
-                <Text>Screen</Text>
-            </View>
+export default class ChatScreen extends Component {
+    state = {
+        messages: []
+    };
+
+    get user() {
+        return {
+            _id: Fire.uid,
+            name: this.props.navigation.state.params.name
+        }
+    }
+
+    componentDidMount() {
+        Fire.get(message => this.setState(previous => ({
+                messages: GiftedChat.append(previous.messages, message)
+            }))
         );
     }
-}
 
-
-const style = StyleSheet.create({
-    container :{
-        flex:1,
-        justifyContent: "center",
-        alignItems: "center"
+    componentWillUnmount() {
+        Fire.off();
     }
-});
+
+    render() {
+        const chat = <GiftedChat messages={this.state.messages} onSend={Fire.send} user={this.user}/>
+
+        if (Platform.OS === 'android') {
+            return (
+                <KeyboardAvoidingView style={{flex: 1}} behavior="padding" keyboardVerticalOffset={30} enabled>
+                    {chat}
+                </KeyboardAvoidingView>
+            )
+        }
+        return<SafeAreaView style={{flex: 1}}>{chat}</SafeAreaView>
+    }
+}
